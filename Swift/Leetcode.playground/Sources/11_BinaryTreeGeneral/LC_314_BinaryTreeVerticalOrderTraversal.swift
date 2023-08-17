a0cfd06526c915f1f5db1851c0e81f2ef954b2bc
viktyz <yujiuqie@163.com>
2023-08-17 16:25:33 +0800

    update

--- Swift/Leetcode.playground/Sources/11_BinaryTreeGeneral/LC_314_BinaryTreeVerticalOrderTraversal.swift
+++ Swift/Leetcode.playground/Sources/11_BinaryTreeGeneral/LC_314_BinaryTreeVerticalOrderTraversal.swift
@@ -13,6 +13,20 @@
  *     }
  * }
  */
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
 class Solution_314 {
     func verticalOrder(_ root: TreeNode?) -> [[Int]] {
         guard let root = root else {
