8870c514e84786c74b90f1fca4d42261c31e6885
viktyz <yujiuqie@163.com>
2023-09-03 18:31:36 +0800

    update

--- parseLCName.java
+++ parseLCName.java
@@ -1,49 +1,56 @@
 import java.util.*;
 
-class parseLCName{
+class parseLCName {
     public static void main(String[] args) {
         String name = "1004. Max Consecutive Ones III";
-        print(name);
+        printJava(name);
+        // printSwift(name);
+        // printJavaScript(name);
     }
-    
-    private static void print(String enName){
-        System.out.println(enName);
+
+    private static void printJava(String enName) {
+        String[] res = parse(enName);
+        System.out.println("|[" + res[0] + "](" + res[1] + ")| [Swift] | [Java](LC_" + res[2] + ".java) | [JavaScript] | ");
+    }
+
+    private static void printSwift(String enName) {
+        String[] res = parse(enName);
+        System.out.println("|[" + res[0] + "](" + res[1] + ")| [Swift](LC_" + res[2] + ".swift) | [Java] | [JavaScript] | ");
+    }
+
+    private static void printJavaScript(String enName) {
+        String[] res = parse(enName);
+        System.out.println("|[" + res[0] + "](" + res[1] + ")| [Swift] | [Java] | [JavaScript](LC_" + res[2] + ".js) | ");
+    }
+
+    private static String[] parse(String enName) {
         String[] indexAndName = (enName.replace('.', ' ')).split(" ");
         int len = indexAndName.length;
         String[] res = new String[len - 2];
-        for(int i = 2; i < len;i++){
+        for (int i = 2; i < len; i++) {
             res[i - 2] = indexAndName[i].toLowerCase();
         }
+
         String fullUrl = "https://leetcode.cn/problems/" + String.join("-", res) + '/';
-        System.out.println("[" + enName + "](" + fullUrl + ")");
 
         String fullName = indexAndName[0] + "_";
-        for(int i = 1; i < len; i++){
+        for (int i = 1; i < len; i++) {
             String str = indexAndName[i];
-            if(str.length() == 0){
+            if (str.length() == 0) {
                 continue;
             }
 
             String tName = "";
 
-            if(i == len - 1){
-                if(str.length() < 5){
-                    Set<Character> set = new HashSet<>();
-                    for(char c : str.toCharArray()){
-                        set.add(c);
-                    }
-                    if(set.size() <= 2 && (set.contains('I') || set.contains('V') )){
-                        tName = str;
-                    }
-                }
+            if (str.equals(str.toUpperCase())) {
+                tName = str;
+            } else {
+                tName = Character.toUpperCase(str.charAt(0)) + str.substring(1);
             }
 
-            tName = Character.toUpperCase(str.charAt(0)) + str.substring(1);
             fullName = fullName + tName;
         }
 
-        System.out.println("LC_"+fullName+".java");
-        System.out.println("LC_"+fullName+".swift");
-        System.out.println("LC_"+fullName+".js");
+        return new String[] { enName, fullUrl,fullName };
     }
 }
