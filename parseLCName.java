b18b77974ed7fe3ed63e9e969522dc31ec847a54
viktyz <yujiuqie@163.com>
2023-09-04 20:49:54 +0800

    update

--- parseLCName.java
+++ parseLCName.java
@@ -1,6 +1,6 @@
 class parseLCName {
     public static void main(String[] args) {
-        String name = "1768. Merge Strings Alternately";
+        String name = "1071. Greatest Common Divisor of Strings";
         printJava(name);
         // printSwift(name);
         // printJavaScript(name);
