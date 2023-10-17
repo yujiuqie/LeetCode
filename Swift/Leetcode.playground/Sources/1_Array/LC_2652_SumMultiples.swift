5e8aa8a26e6a01320bee0922d874d8ea5ae2f9a6
yujiuqie <yujiuqie@163.com>
2023-10-17 16:29:47 +0800

    update

---
+++ Swift/Leetcode.playground/Sources/1_Array/LC_2652_SumMultiples.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func sumOfMultiples(_ n: Int) -> Int {
+        var sum = 0
+        for i in 0...n{
+            if i % 3 == 0 || i % 5 == 0 || i % 7 == 0{
+                sum += i
+            }
+        }
+        return sum
+    }
+}
